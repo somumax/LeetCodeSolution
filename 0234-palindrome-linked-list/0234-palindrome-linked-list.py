fa2fa4ed96dbbcc11c23c7e057dbb3adb4e3255f
somumax <storeariya72@gmail.com>
2025-03-27 13:24:26 +0530

    Time: 31 ms (65.27%), Space: 39.3 MB (38.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,28 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return True
-        #Find the middle element
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        #Reverse the second half
-        prev, curr = None, slow
+        traversal = []
+        curr = head 
         while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        #Compair two halves
-        first, second = head, prev
-        while second:
-            if first.val!=second.val:
+            traversal.append(curr.val)
+            curr = curr.next
+        for i in range(len(traversal) // 2):
+            if (traversal[i] != traversal[len(traversal) - 1 - i]):
                 return False
-            first = first.next
-            second = second.next
         return True
+        
         
